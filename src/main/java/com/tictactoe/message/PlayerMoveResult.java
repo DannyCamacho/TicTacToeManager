f1294a8569c62de26d21ad43ef084c7fcfdf8df9
Daniel Camacho <dcamacho4@saddleback.edu>
2022-05-09 04:49:28 -0700

    Update to manager

--- src/main/java/com/tictactoe/message/PlayerMoveResult.java
+++ src/main/java/com/tictactoe/message/PlayerMoveResult.java
@@ -2,4 +2,4 @@ package com.tictactoe.message;
 
 import java.io.Serializable;
 
-public record PlayerMoveResult(String gameName, char playerToken, int move, String result, char [] board) implements Serializable {}
+public record PlayerMoveResult(String gameName, char playerToken, String result, char [] board) implements Serializable {}
